a5b833561d3a5abd11304d98604dc09d8deb23e1
Andrzej Borucki <borucki.andrzej@gmail.com>
2025-06-24 14:10:51 +0200

    init batch downloader

---
+++ dump/getWithIndex.sh
@@ -0,0 +1,3 @@
+read -r DATE < DATE
+wget -c https://mirror.accum.se/mirror/wikimedia.org/dumps/$1/$DATE/$1-$DATE-pages-articles-multistream.xml.bz2
+wget -c https://mirror.accum.se/mirror/wikimedia.org/dumps/$1/$DATE/$1-$DATE-pages-articles-multistream-index.txt.bz2
---
+++ dump/run.sh
@@ -0,0 +1,9 @@
+curl -s https://mirror.accum.se/mirror/wikimedia.org/dumps/afwiktionary/ \
+  | grep -o '<a href="20[0-9]\{6\}/"' \
+  | cut -d'"' -f2 | cut -d'/' -f1 \
+  | sort | tail -n1 > DATE
+curl -s https://mirror.accum.se/mirror/wikimedia.org/dumps/ | grep wiktionary | grep -oE '[a-z]+wiktionary' | sort | uniq > list.txt
+echo "#!/bin/bash" > getall.sh
+awk '{print "./getWithIndex.sh", $0}' list.txt >> getall.sh
+chmod +x getall.sh
+./getall.sh

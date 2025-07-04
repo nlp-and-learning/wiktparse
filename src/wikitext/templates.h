e83f32ae87ad444d22a6c900fd10dca3dac98e80
Andrzej Borucki <borucki.andrzej@gmail.com>
2025-07-04 17:53:24 +0200

    chore: add extractTemplates header

--- src/wikitext/templates.h
+++ src/wikitext/templates.h
@@ -3,4 +3,5 @@
 #include <vector>
 namespace fs = std::filesystem;
 
+std::vector<std::string> extractTemplates(const std::string& text);
 std::vector<std::string> extractTemplatesFromFile(const fs::path& path);

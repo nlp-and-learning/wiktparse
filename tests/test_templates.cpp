934f310df69781dcee2bea3f09fc34a6441e8698
Andrzej Borucki <borucki.andrzej@gmail.com>
2025-07-04 21:47:46 +0200

    EXPECT_EQ - first expected, next computed parameter

--- tests/test_templates.cpp
+++ tests/test_templates.cpp
@@ -11,38 +11,38 @@ std::string parseAndPrint(const std::string& input) {
 TEST(TemplateParserTest, BasicTemplate) {
     std::string input = "{{test|foo|bar=baz}}";
     std::string expected = "{{test|foo|bar=baz}}";
-    EXPECT_EQ(parseAndPrint(input), expected);
+    EXPECT_EQ(expected, parseAndPrint(input));
 }
 
 TEST(TemplateParserTest, NestedTemplate) {
     std::string input = "{{outer|{{inner|val}}|key=value}}";
     std::string expected = "{{outer|{{inner|val}}|key=value}}";
-    EXPECT_EQ(parseAndPrint(input), expected);
+    EXPECT_EQ(expected, parseAndPrint(input));
 }
 
 TEST(TemplateParserTest, ParserFunction) {
     std::string input = "{{#expr: 2+2}}";
     std::string expected = "{{#expr:2+2}}";
-//    EXPECT_EQ(parseAndPrint(input), expected);
+//    EXPECT_EQ(expected, parseAndPrint(input));
 }
 
 TEST(TemplateParserTest, WikiLinkSimple) {
     std::string input = "[[France]]";
     size_t pos = 0;
     auto result = parseWikiLink(input, pos);
-    EXPECT_EQ(result->toWikitext(FormatStyle::Compact), "[[France]]");
+    EXPECT_EQ("[[France]]", result->toWikitext(FormatStyle::Compact));
 }
 
 TEST(TemplateParserTest, WikiLinkWithAlias) {
     std::string input = "[[France|French Republic]]";
     size_t pos = 0;
     auto result = parseWikiLink(input, pos);
-    EXPECT_EQ(result->toWikitext(FormatStyle::Compact), "[[France|French Republic]]");
+    EXPECT_EQ("[[France|French Republic]]", result->toWikitext(FormatStyle::Compact));
 }
 
 TEST(TemplateParserTest, CompositeWithMultipleLinks) {
     std::string input = "[[France]], [[Belgium]] and [[Canada]]";
     size_t pos = 0;
     auto result = parseCompositeText(input, pos, false);
-    EXPECT_EQ(result->toWikitext(FormatStyle::Compact), "[[France]], [[Belgium]] and [[Canada]]");
+    EXPECT_EQ("[[France]], [[Belgium]] and [[Canada]]", result->toWikitext(FormatStyle::Compact));
 }

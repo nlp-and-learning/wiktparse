6b0be4ad5e1477bdfe4784c87653f7f3c526fa8c
Andrzej Borucki <borucki.andrzej@gmail.com>
2025-07-06 10:44:40 +0200

    refactor: removed compact whitespaces from TextParser::parse ()

--- src/wikitext/TextParser.h
+++ src/wikitext/TextParser.h
@@ -9,25 +9,7 @@ public:
     std::unique_ptr<Fragment> parse() {
         auto composite = std::make_unique<CompositeText>();
         std::ostringstream ss;
-        size_t outBreaks = 0;
-        size_t outSpaces = 0;
-        while (true) {
-            auto p = skipWhiteBreaks();
-            if (pos >= text.size()) break;
-            if (commentStatus == CommentStatus::Displayed) {
-                outBreaks = 0;
-                outSpaces = 0;
-            }
-            if (p.first > 1) {
-                std::string breaks(p.first - 1, '\n');
-                if (commentStatus == CommentStatus::Displayed)
-                    outBreaks = p.first - 1;
-                ss << breaks;
-            } else if (p.first + p.second > 0) {
-                if (commentStatus == CommentStatus::Displayed)
-                    outSpaces = 1;
-                ss << ' ';
-            }
+        while (pos < text.size()) {
             auto spec = specialAt();
             if (spec == StartSpecial::CommentOpen) {
                 if (commentStatus == CommentStatus::Displayed) {
@@ -46,14 +28,6 @@ public:
                     composite->parts.emplace_back(std::make_unique<TextFragment>(commented, commentStatus));
                     ss.str("");
                     commentStatus = CommentStatus::Displayed;
-                    auto p1 = skipWhiteBreaks();
-                    if (p1.first > 0 && outBreaks > 0 || p1.first > 1 && outBreaks == 0) {
-                        std::string breaks(p1.first, '\n');
-                        ss << breaks;
-                    } else if (p1.first + p1.second > 0) {
-                        if (outSpaces==0)
-                            ss << ' ';
-                    }
                     if (pos >= text.size()) break;
                 }
             }

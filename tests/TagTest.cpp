dd3ca093ad1af83b83118dab7392d3aff63ac0a9
Andrzej Borucki <borucki.andrzej@gmail.com>
2025-07-07 20:29:05 +0200

    test: tag "<br  !/>"

--- tests/TagTest.cpp
+++ tests/TagTest.cpp
@@ -11,6 +11,15 @@ TEST(TagTest, SelfClosing) {
     EXPECT_EQ(0, tag->attributes.size());
 }
 
+TEST(TagTest, OtherChar) {
+    std::string input = "<br  !/>";
+    TagParser parser(input, 0);
+    auto tag = parser.parse();
+    EXPECT_EQ(TagType::SelfClosing, tag->type);
+    EXPECT_EQ("br", tag->name);
+    EXPECT_EQ(0, tag->attributes.size());
+}
+
 TEST(TagTest, Attributes) {
     std::string input = R"(<span style="color:red;" class="highlight" title="example">)";
     TagParser parser(input, 0);

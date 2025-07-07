a848b3402b1332c053d41c59e73011a43e64171f
Andrzej Borucki <borucki.andrzej@gmail.com>
2025-07-07 21:19:14 +0200

    refactor: moved isWikiSpace,isBreak,skipWhitespace,skipLineBreaks,skipWhiteBreaks to class Whitespace

--- src/markup/Whitespace.h
+++ src/markup/Whitespace.h
@@ -13,4 +13,40 @@ struct Whitespace {
             ++pos;
         return pos - start;
     }
+
+    static bool isBreak(char c) {
+        return c=='\r' || c=='\n';
+    }
+
+    static size_t skipLineBreaks(const std::string& text, size_t& pos) {
+        size_t countCR = 0;
+        size_t countLF = 0;
+        while (pos < text.size()) {
+            if (text[pos] == '\r') {
+                ++countCR;
+                ++pos;
+            } else if (text[pos] == '\n') {
+                ++countLF;
+                ++pos;
+            } else {
+                break;
+            }
+        }
+        auto countBreak = std::max(countCR, countLF);
+        return countBreak;
+    }
+
+    static std::pair<size_t, size_t> skipWhiteBreaks(const std::string& text, size_t& pos) {
+        size_t breakCounter = 0;
+        size_t whiteCounter = 0;
+        while(pos < text.size()) {
+            if (isBreak(text[pos]))
+                breakCounter += skipLineBreaks(text, pos);
+            else if (isWikiSpace(text[pos]))
+                whiteCounter += skipWhitespace(text, pos);
+            else break;
+        }
+        return {breakCounter, whiteCounter};
+    }
+
 };
--- src/tags/BaseParser.h
+++ src/tags/BaseParser.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstring>
+#include "../markup/Whitespace.h"
 
 enum class StartSpecial {
     Tag,
@@ -33,7 +34,7 @@ protected:
                     return StartSpecial::Other;
             }
             case '=': {
-                if (pos == 0 || isBreak(text[pos - 1]))
+                if (pos == 0 || Whitespace::isBreak(text[pos - 1]))
                     return StartSpecial::Header;
                 else
                     return StartSpecial::Other;
@@ -46,48 +47,6 @@ protected:
             }
             default: return StartSpecial::Other;
         }
-    }
-
-    static bool isWikiSpace(char c) {
-        return std::strchr(" \t\f\v", static_cast<unsigned char>(c)) != nullptr;
-    }
-    static bool isBreak(char c) {
-        return c=='\r' || c=='\n';
-    }
-    size_t skipWhitespace() {
-        auto start = pos;
-        while (pos < text.size() && isWikiSpace(text[pos]))
-            ++pos;
-        return pos - start;
-    }
-    size_t skipLineBreaks() {
-        size_t countCR = 0;
-        size_t countLF = 0;
-        while (pos < text.size()) {
-            if (text[pos] == '\r') {
-                ++countCR;
-                ++pos;
-            } else if (text[pos] == '\n') {
-                ++countLF;
-                ++pos;
-            } else {
-                break;
-            }
-        }
-        auto countBreak = std::max(countCR, countLF);
-        return countBreak;
-    }
-    std::pair<size_t, size_t> skipWhiteBreaks() {
-        size_t breakCounter = 0;
-        size_t whiteCounter = 0;
-        while(pos < text.size()) {
-            if (isBreak(text[pos]))
-                breakCounter += skipLineBreaks();
-            else if (isWikiSpace(text[pos]))
-                whiteCounter += skipWhitespace();
-            else break;
-        }
-        return {breakCounter, whiteCounter};
     }
 public:
     BaseParser(const std::string& text, size_t pos) : text(text),pos(pos) {}

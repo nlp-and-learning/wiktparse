68850c84561befa08de70ff9bd60cc643fb7b70e
Andrzej Borucki <borucki.andrzej@gmail.com>
2025-07-10 18:14:44 +0200

    test: all NowikiTest uses Comments::preparse(input)->str()

--- tests/CommentTest.cpp
+++ tests/CommentTest.cpp
@@ -8,54 +8,54 @@ TEST(CommentTest, HideComments) {
     std::string expected = "This is simple text";
     std::string comments = "<!--comment 1--><!--comment 2-->";
 
-    EXPECT_EQ(expected, Comments::clean(input));
+    EXPECT_EQ(expected, Comments::preparse(input)->str());
 }
 
 TEST(CommentTest, CommentAtEnd) {
     std::string input = "This is simple text<!--comment";
     std::string expected = "This is simple text";
 
-    EXPECT_EQ(expected, Comments::clean(input));
+    EXPECT_EQ(expected, Comments::preparse(input)->str());
 }
 
 TEST(CommentTest, CommentAtEnd2) {
     std::string input = "This is simple --> text<!--comment";
     std::string expected = "This is simple --> text";
 
-    EXPECT_EQ(expected, Comments::clean(input));
+    EXPECT_EQ(expected, Comments::preparse(input)->str());
 }
 
 TEST(CommentTest, CommentInsideNowiki) {
     std::string input = "This is <nowiki><!--comment--></nowiki> text";
     std::string expected = "This is <!--comment--> text";
 
-    EXPECT_EQ(expected, Comments::clean(input));
+    EXPECT_EQ(expected, Comments::preparse(input)->str());
 }
 
 TEST(CommentTest, CommentPartInsideNowiki) {
     std::string input = "This is <nowiki><!--comm</nowiki>ent--> text";
     std::string expected = "This is <!--comment--> text";
 
-    EXPECT_EQ(expected, Comments::clean(input));
+    EXPECT_EQ(expected, Comments::preparse(input)->str());
 }
 
 TEST(CommentTest, NowikiInsideCommentPart) {
     std::string input = "This is <!--comm<nowiki>>ent--></nowiki text";
     std::string expected = "This is </nowiki text";
 
-    EXPECT_EQ(expected, Comments::clean(input));
+    EXPECT_EQ(expected, Comments::preparse(input)->str());
 }
 
 TEST(CommentTest, NowikiInsideComment) {
     std::string input = "This is <!--comment <nowiki> abc <nowiki> --> text";
     std::string expected = "This is  text";
 
-    EXPECT_EQ(expected, Comments::clean(input));
+    EXPECT_EQ(expected, Comments::preparse(input)->str());
 }
 
 TEST(CommentTest, CommentInsidePre) {
     std::string input = "This is <pre><!--comment--></pre> text";
     std::string expected = "This is <pre></pre> text";
 
-    EXPECT_EQ(expected, Comments::clean(input));
+    EXPECT_EQ(expected, Comments::preparse(input)->str());
 }
